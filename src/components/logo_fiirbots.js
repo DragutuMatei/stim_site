5bfab2414b0136bd66e0f27d74d464509ff25896
Mihail Hanga <51721673+ZuZu-Mihail@users.noreply.github.com>
2026-02-18 00:34:15 +0200

    update

---
+++ src/components/logo_fiirbots.js
@@ -0,0 +1,11 @@
+
+// compontents of logo
+import { ReactComponent as FiirbotsLogo } from '../assets/logo_fiirbots.svg';
+
+function LogoFiirbots() {
+    return (
+        <FiirbotsLogo />
+    )
+}
+
+export default LogoFiirbots;
---
+++ src/components/logo_stim.js
@@ -0,0 +1,11 @@
+
+// compontents of logo
+import { ReactComponent as STIM_Logo } from '../assets/logo_stim.svg';
+
+function LogoSTIM() {
+    return (
+        <STIM_Logo />
+    )
+}
+
+export default LogoSTIM;
